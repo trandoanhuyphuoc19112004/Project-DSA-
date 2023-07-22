53753a2d81df1b9a544c2e9c8b25d17bd403a663
hohanhi <hanhi11062004@gmail.com>
2023-07-22 19:52:49 +0700

    command 1

---
+++ Project/Main.cpp
@@ -0,0 +1,169 @@
+#include"Header.h"
+#include "Cmdheader.h"
+#include "Gerenatedata.h"
+#include "Copydata.h"
+int main(int argc, char **argv)
+{
+	 bool flag = true; 
+	string arrAlgorithm[11] = {"selection-sort", "bubble-sort", "counting-sort", "insertion-sort", "shaker-sort", "shell-sort", "quick-sort", "radix-sort", "heap-sort", "flash-sort", "merge-sort"};
+ 
+	if (argc <=4 )
+	{
+		flag = false; 
+	}
+	else if (argc == 5)
+	{
+		
+		//Algorithm mode
+		if (!strcmp(argv[1], "-a")) {
+			//Command 1
+			if (argv[3][strlen(argv[3] - 1)] >= 'a' && argv[3][strlen(argv[3] - 1)] <= 'z') {
+				string name_algo = argv[2];
+				char* output_param = argv[4];
+				const char* filename = (const char*)argv[3];
+				flag = checkAlgorithm1(arrAlgorithm, 11, name_algo);
+				if (flag)
+					command1(filename, name_algo, output_param);
+			}
+		}
+		
+
+		// Comparison mode
+		if(!strcmp(argv[1], "-c"))
+		{
+			string name_algo1 = argv[2]; 
+			string name_algo2 = argv[3];
+			const char* filename = (const char*)argv[4];
+			flag = checkAlgorithm2(arrAlgorithm, 11, name_algo1, name_algo2);
+			if (flag)
+			{
+				Comparison_inputfile(filename, name_algo1, name_algo2); 
+			}
+		}
+	}
+	else if (argc == 6)
+	{
+		// Comparison mode
+		if(!strcmp(argv[1], "-c"))
+		{
+			string name_algo1 = argv[2]; 
+			string name_algo2 = argv[3];
+			int size = stoi(argv[4]);
+			flag = checkAlgorithm2(arrAlgorithm, 11, name_algo1, name_algo2);
+			if (flag && size < 1000000)
+			{
+				Comparison_inputsize_order(name_algo1, name_algo2, size, argv[5]);
+			}
+		}
+
+	}
+	if(!flag)
+	{
+		cout << "Syntax error " << endl;
+		exit(0); 
+	}
+}
+	// int size = 0;
+	// unsigned long long cp = 0;
+	// unsigned long long time = 0;
+	// // int* selectionsort_arr;
+	// // int* insertionsort_arr;
+	// // int* bubblesort_arr;
+	// // int* heapsort_arr;
+	// // int* mergesort_arr;
+	// // int* quicksort_arr;
+	// // int* radixsort_arr;
+	// // int* shakersort_arr;
+	// // int* shellsort_arr;
+	// // int* countingsort_arr;
+	// // int* flashsort_arr;
+	// int* arr;
+	// copyAndGenerateData(arr, size, 0, 1);
+	// savefile("Text.txt", arr, size); 
+	// deleteData(arr);
+
+	// Display output
+	
+	// cout << ">Comparision and time using<" << endl;
+	// cout << "============================" << endl;
+	// selectionSort(selectionsort_arr, size, time, cp);
+	// cout << "Selection sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// insertionSort(insertionsort_arr, size, time, cp);
+	// cout << "Insertion sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+
+	// cout << "============================" << endl;
+	// bubbleSort(bubblesort_arr, size, time, cp); 
+	// cout << "Bubble sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+	
+	// heapSort(heapsort_arr, size, time, cp); 
+	// cout << "Heap sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// mergeSort(mergesort_arr, size, time, cp);
+	// cout << "Merge sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// quickSort(quicksort_arr, size, time, cp); 
+	// cout << "Quick sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// radixSort(flashsort_arr, size, time, cp);
+	// cout << "Radix sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// shakerSort(shakersort_arr, size, time, cp); 
+	// cout << "Shaker sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// shellSort(shellsort_arr, size, time, cp); 
+	// cout << "Shell sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// countingSort(countingsort_arr, size, time, cp); 
+	// cout << "Counting sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// flashSort(flashsort_arr, size, time, cp); 
+	// cout << "Flash sort" << endl;
+	// cout << "Comparision: " << cp << endl;
+	// cout << "Time using (micro second): " << time << endl;
+	// cout << "============================" << endl;
+
+	// deleteData(selectionsort_arr);
+	// deleteData(insertionsort_arr);
+	// deleteData(bubblesort_arr);
+	// deleteData(heapsort_arr);
+	// deleteData(mergesort_arr);
+	// deleteData(quicksort_arr);
+	// deleteData(radixsort_arr);
+	// deleteData(shakersort_arr);
+	// deleteData(shellsort_arr);
+	// deleteData(countingsort_arr);
+	// deleteData(flashsort_arr);
+
+
+
+	
---
+++ Project/cmdheader.cpp
@@ -0,0 +1,193 @@
+#include "Cmdheader.h"
+void (*SORT_ALGO[11])(int *a, int size, unsigned long long &time, unsigned long long &comparison) = {selectionSort, insertionSort, bubbleSort, heapSort, mergeSort, quickSort, radixSort, shakerSort, shellSort, countingSort, flashSort};
+bool checkAlgorithm1(string name[], int number,  string inputname1)
+{
+    bool flag = false; 
+   
+    // Check inputname1 
+    for (int i = 0; i < number; i++)
+    {
+        if (name[i] == inputname1)
+            {
+                flag = true; 
+                break; 
+            }
+    }
+    if (flag)
+        return true;
+    return false; 
+}
+
+bool checkAlgorithm2(string name[], int number,  string inputname1, string inputname2)
+{
+    bool flag1 = false; 
+    bool flag2 = false; 
+    // Check inputname1 
+    for (int i = 0; i < number; i++)
+    {
+        if (name[i] == inputname1)
+            {
+                flag1 = true; 
+                break; 
+            }
+    }
+    // Check inputname2
+     for (int i = 0; i < number; i++)
+    {
+        if (name[i] == inputname2)
+            {
+                flag2 = true; 
+                break; 
+            }
+    }
+    if (flag1 && flag2)
+        return true;
+     return false; 
+}
+
+// Ref: https://github.com/tonip1z/Lab03_DSA_Group03/blob/master/SOURCE/Experiment.cpp
+int getAlgoId(const char* algo_name)
+{
+    if (strcmp(algo_name, "selection-sort") == 0)
+        return 0;
+    if (strcmp(algo_name, "insertion-sort") == 0)
+        return 1;
+    if (strcmp(algo_name, "bubble-sort") == 0)
+        return 2;
+    if (strcmp(algo_name, "heap-sort") == 0)
+        return 3;
+    if (strcmp(algo_name, "merge-sort") == 0)
+        return 4;
+    if (strcmp(algo_name, "quick-sort") == 0)
+        return 5;
+    if (strcmp(algo_name, "radix-sort") == 0)
+        return 6;
+    if (strcmp(algo_name, "shaker-sort") == 0)
+        return 7;
+    if (strcmp(algo_name, "shell-sort") == 0)
+        return 8;
+    if (strcmp(algo_name, "counting-sort") == 0)
+        return 9;
+    if (strcmp(algo_name, "flash-sort") == 0)
+        return 10;
+    
+    return -1;
+}
+
+int getinputorderId(char* order_name)
+{
+    if (strcmp(order_name, "-rand") == 0)
+        return 0;
+    if (strcmp(order_name, "-sorted") == 0)
+        return 1;
+    if (strcmp(order_name, "-rev") == 0)
+        return 2;
+    if (strcmp(order_name, "-nsorted") == 0)
+        return 3;
+    return -1;
+}
+
+void command1(const char* filename, string name_algo, char* output_param) {
+    unsigned long long time, comparison;
+    int algoID = getAlgoId(name_algo.c_str());
+    int* arr = nullptr;
+    int size = 0;
+
+    cout << "ALGORITHM MODE:" << endl;
+    cout << "Algorithm: " << name_algo << endl;
+    bool flag = readfile(filename, arr, size);
+    if (!flag)
+        return;
+
+    (*SORT_ALGO[algoID])(arr, size, time, comparison);
+    cout << "Input file: " << filename << endl;
+    cout << "Input size: " << size << endl;
+    cout << "-------------------------" << endl;
+    if (strcmp(output_param, "-both") == 0) {
+        cout << "Running time: " << time << endl;
+        cout << "Comparison: " << comparison << endl;
+    } 
+    else if (strcmp(output_param, "-time") == 0) {
+        cout << "Running time: " << time << endl;
+    }
+    else if (strcmp(output_param, "-comp") == 0) {
+        cout << "Comparison: " << comparison << endl;
+    }
+
+    delete[] arr;
+}
+
+//Command 4
+void Comparison_inputfile(const char* filename, string name_algo1, string name_algo2)
+{
+    unsigned long long time1, time2; 
+    unsigned long long compare1, compare2;
+
+    int algoID1 = getAlgoId(name_algo1.c_str());
+    int algoID2 = getAlgoId(name_algo2.c_str()); 
+
+    int *arr1 = nullptr;
+    int *arr2 = nullptr; 
+
+    int size1 = 0, size2 = 0; 
+
+    cout << "COMPARE MODE:" << endl; 
+    cout << "Algorithm: " << name_algo1 << "|" << name_algo2 << endl;
+
+    bool flag1 =  readfile(filename, arr1, size1); 
+    bool flag2 = readfile(filename, arr2, size2);
+    if (!flag1 || !flag2)
+        return; 
+
+    (*SORT_ALGO[algoID1])(arr1, size1, time1, compare1); 
+    (*SORT_ALGO[algoID2])(arr2, size2, time2, compare2); 
+
+    cout << "Input size:" << size1 << endl; 
+    cout << "-------------------------" << endl; 
+    cout << "Running time:" << time1 << "|" << time2 << endl;
+    cout << "Comparisons:" << compare1 << "|" << compare2 << endl; 
+
+    delete [] arr1; 
+    delete [] arr2; 
+}
+
+//Command 5
+void Comparison_inputsize_order(string name_algo1, string name_algo2, int inputsize, char* inputorder)
+{
+    unsigned long long time1, time2; 
+    unsigned long long compare1, compare2;
+
+    int algoID1 = getAlgoId(name_algo1.c_str());
+    int algoID2 = getAlgoId(name_algo2.c_str()); 
+
+    int *arr1 = nullptr; 
+    int *arr2 = nullptr; 
+    int order_index = getinputorderId(inputorder); 
+
+
+    if (order_index == -1)
+    {
+        cout << "Unvalid order" << endl; 
+        return; 
+    }
+
+    copyAndGenerateData(arr1, inputsize, order_index); 
+    copyAndGenerateData(arr2, inputsize, order_index);
+
+    savefile("input.txt", arr1, inputsize); 
+
+    (*SORT_ALGO[algoID1])(arr1, inputsize, time1, compare1); 
+    (*SORT_ALGO[algoID2])(arr2, inputsize, time2, compare2); 
+
+    cout << "COMPARE MODE:" << endl; 
+    cout << "Algorithm: " << name_algo1 << "|" << name_algo2 << endl;
+    cout << "Input size:" << inputsize << endl; 
+
+    cout << "Input order:" << inputorder << endl; 
+    cout << "-------------------------" << endl; 
+    cout << "Running time:" << time1 << "|" << time2 << endl;
+    cout << "Comparisons:" << compare1 << "|" << compare2 << endl; 
+
+    delete [] arr1; 
+    delete [] arr2; 
+}
